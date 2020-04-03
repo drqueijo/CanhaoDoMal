b8a428919725a71f9f2539925693809d30234b8d
drqueijo <silveriomat1234@gmail.com>
2020-04-03 11:41:51 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1585923001,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/flor.png",
+		"images/ball-sheet0.png",
+		"images/box-sheet0.png",
+		"images/cannon-sheet0.png",
+		"images/cannon2-sheet0.png",
+		"images/particles.png",
+		"images/tnt-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/particles2.png",
+		"images/particles3.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
